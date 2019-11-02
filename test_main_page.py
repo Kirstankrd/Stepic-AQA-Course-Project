d704650f6ab05f24655f89f63441a4ec53584e18
Nikolay Kirichenko <n.kirichenko@vezet.ru>
2019-11-02 16:10:06 +0300

    Refactor test_main_page to PageObject test

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,8 @@
-url = "http://selenium1py.pythonanywhere.com/"
+from pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    browser.get(url)
-    go_to_login_page(browser)
-
-
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
+def test_guest_can_do_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
