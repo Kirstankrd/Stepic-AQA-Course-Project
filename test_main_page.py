4e9dc97d09572c46bde524e36889f1108bb9a1b3
Nikolay Kirichenko <n.kirichenko@vezet.ru>
2019-11-02 18:06:48 +0300

    Tests for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_do_to_login_page(browser):
@@ -13,3 +14,13 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_can_see_login_and_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+
+
+
